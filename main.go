1fd4b7ae4439685607caf7db6888387657b6884c
tangtao <412145540@qq.com>
2016-05-11 19:37:32 +0800

    init

--- main.go
+++ main.go
@@ -13,7 +13,7 @@ var db *sql.DB
 
 func init() {
 	log.Println("dsdsd");
-	db, _ = sql.Open("mysql", "tangtao:123456@tcp(172.30.121.158:3307)/sampledb?charset=utf8")
+	db, _ = sql.Open("mysql", "tangtao:123456@tcp(172.30.121.158:3306)/sampledb?charset=utf8")
 	db.SetMaxOpenConns(2000)
 	db.SetMaxIdleConns(1000)
 	db.Ping()
