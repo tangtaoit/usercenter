44cda4a00d889272aaed28a303db266940ebfb5c
tangtao <412145540@qq.com>
2016-05-11 17:57:34 +0800

    init

--- main.go
+++ main.go
@@ -3,24 +3,45 @@ package main
 import (
 	"net/http"
 	"io"
+	"fmt"
+	"usercenter/db"
 )
 
-type User struct {
-	ID     int
-	Name   string
-	Colors []string
-}
 
 func main() {
 	//mux:=httpim.NewServeMux();
+
 
 	http.HandleFunc("/user/login", userLogin)
 
 	http.ListenAndServe(":8080", nil)
 }
 
+func CheckErr(err error)  {
+	if err != nil {
+		panic(err)
+	}
+}
+
 func  userLogin(w http.ResponseWriter, r *http.Request){
 
-	io.WriteString(w,"testdd");
+
+	rows, err := db.Get().Query("select id,rid,appid from users")
+
+	defer rows.Close()
+
+	CheckErr(err)
+
+	var resultStr string
+	for rows.Next() {
+		var id int
+		var rid string
+		var appid int
+		err = rows.Scan(&id,&rid,&appid)
+		CheckErr(err)
+		resultStr =fmt.Sprint(id,"-",rid,"-",appid)
+	}
+
+	io.WriteString(w,resultStr);
 
 }
